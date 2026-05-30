7a9828354595afcc0f2bc879b71b67816199ae95
gfrancine <gracefrancine.t@gmail.com>
2026-05-30 21:30:05 +1000

    feat: 4-up saddle stitch tall booklet preset

--- src/presets/index.ts
+++ src/presets/index.ts
@@ -1,6 +1,7 @@
 import businessCard from "./business-card-8up";
 import addTrimMarks from "./add-trim-marks";
 import saddleStitch from "./saddle-stitch-2up";
+import saddleStitch4UpTall from "./saddle-stitch-4up-tall";
 import removeInnerBleed from "./remove-inner-bleed";
 
 export const presets = {
@@ -8,6 +9,7 @@ export const presets = {
   removeInnerBleed,
   businessCard,
   saddleStitch,
+  saddleStitch4UpTall,
 };
 
 export type PresetId = keyof typeof presets;
---
+++ src/presets/saddle-stitch-4up-tall.ts
@@ -0,0 +1,131 @@
+/*
+
+Saddle-Stitched Tall Booklet 4-Up
+
+*/
+
+import { PDFDocument } from "pdf-lib";
+import {
+  calcExtraGutter,
+  drawSpread,
+  mapIndicesSaddleStitch,
+  toPts,
+  Vec2,
+  type SaddleStitchIndexGroup,
+} from "../utils";
+import type { Preset } from "../types";
+
+const name = "Saddle-Stitched Tall Booklet 4-Up";
+
+const DEFAULT_SETTINGS = {
+  sheetWidth: 297,
+  sheetHeight: 210,
+  bleedArea: 5,
+  trimMarkLength: 5,
+  trimMarkOffset: 2,
+};
+
+type Settings = typeof DEFAULT_SETTINGS;
+
+async function impose(
+  srcPdf: PDFDocument,
+  settings: Settings = DEFAULT_SETTINGS,
+) {
+  const outPdf = await PDFDocument.create();
+  const srcPages = await outPdf.embedPages(srcPdf.getPages());
+
+  const sheetSize = new Vec2(
+    toPts(settings.sheetWidth),
+    toPts(settings.sheetHeight),
+  );
+  const sheetCenter = sheetSize.div(2);
+  const bleedArea = toPts(settings.bleedArea);
+  const trimLength = toPts(settings.trimMarkLength);
+  const trimOffset = toPts(settings.trimMarkOffset);
+  const extraGutter = calcExtraGutter(bleedArea, trimLength, trimOffset);
+
+  // validate input & settings
+  // will throw if page count isn't a multiple of 4
+  const indexGroups = mapIndicesSaddleStitch(srcPages.length);
+
+  const calcLeafTotalWidth = (leaf: SaddleStitchIndexGroup) =>
+    // handle pages of  different sizes
+    Math.max(srcPages[leaf.front2].width, srcPages[leaf.back2].width) +
+    Math.max(srcPages[leaf.front1].width, srcPages[leaf.back1].width) +
+    (-bleedArea + trimOffset + trimLength) * 2;
+
+  for (let i = 0; i < indexGroups.length; i += 2) {
+    const frontSheet = outPdf.addPage([sheetSize.x, sheetSize.y]);
+    const backSheet = outPdf.addPage([sheetSize.x, sheetSize.y]);
+
+    const leaf1 = indexGroups[i];
+    const leaf2: SaddleStitchIndexGroup | undefined = indexGroups[i + 1]; // may not exist. test handling with odd leaf count booklets
+
+    // TODO: add boolean options to enable gutter & inner trim marks
+
+    const hideRightHorizTrimMarks = {
+      topRightHoriz: true,
+      bottomRightHoriz: true,
+    };
+
+    const hideLeftHorizTrimMarks = {
+      topLeftHoriz: true,
+      bottomLeftHoriz: true,
+    };
+
+    const leaf1Width = calcLeafTotalWidth(leaf1);
+
+    drawSpread(frontSheet, {
+      origin: sheetCenter.sub(leaf1Width / 2, 0).add(extraGutter, 0),
+      leftPage: srcPages[leaf1.front2],
+      rightPage: srcPages[leaf1.front1],
+      bleedArea,
+      trimLength,
+      trimOffset,
+      hideTrimMarks: hideRightHorizTrimMarks,
+    });
+
+    drawSpread(backSheet, {
+      origin: sheetCenter.add(leaf1Width / 2, 0).sub(extraGutter, 0),
+      leftPage: srcPages[leaf1.back1],
+      rightPage: srcPages[leaf1.back2],
+      bleedArea,
+      trimLength,
+      trimOffset,
+      hideTrimMarks: hideLeftHorizTrimMarks,
+    });
+
+    if (leaf2) {
+      const leaf2Width = calcLeafTotalWidth(leaf2);
+
+      drawSpread(frontSheet, {
+        origin: sheetCenter.add(leaf2Width / 2, 0).sub(extraGutter, 0),
+        leftPage: srcPages[leaf2.front2],
+        rightPage: srcPages[leaf2.front1],
+        bleedArea,
+        trimLength,
+        trimOffset,
+        hideTrimMarks: hideLeftHorizTrimMarks,
+      });
+
+      drawSpread(backSheet, {
+        origin: sheetCenter.sub(leaf2Width / 2, 0).add(extraGutter, 0),
+        leftPage: srcPages[leaf2.back1],
+        rightPage: srcPages[leaf2.back2],
+        bleedArea,
+        trimLength,
+        trimOffset,
+        hideTrimMarks: hideRightHorizTrimMarks,
+      });
+    }
+  }
+
+  return outPdf;
+}
+
+const preset: Preset = {
+  name,
+  impose,
+};
+
+export default preset;
